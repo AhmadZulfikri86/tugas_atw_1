119964f86114148fa5f6921567bc9b3e8f8240e4
AhmadZulfikri86 <71203029+AhmadZulfikri86@users.noreply.github.com>
2020-09-13 15:05:47 +0700

    belajar class and method

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+class mobil {
+    public $merk, $tipe, $mesin, $max_speed;
+
+    public function cetakTipe(){
+        return $this->tipe;
+    }
+    function KecepatanMaksimal() {
+        return "Kecepatan Maksimal Dari Mobil Ini Adalah".$this->max_speed;
+    }
+}
+
+$bmw = new Mobil;
+$bmw ->merk = "BMW"; 
+$bmw ->tipe = "320i";
+$bmw ->mesin = "2000cc";
+$bmw ->max_speed = "280km/h";
+
+echo "Tipe". $bmw->KecepatanMaksimal();
